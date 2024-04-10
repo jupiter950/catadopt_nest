e997ca3b9428a1ec196ec2d81ab579f905a90d56
jupiter950 <venusdragon718@gmail.com>
2024-04-11 03:12:10 +0900

    fix: auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [
@@ -13,5 +15,11 @@ import { FavoritesModule } from './favorites/favorites.module';
     UsersModule,
     FavoritesModule
   ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ]
 })
 export class AppModule {}
