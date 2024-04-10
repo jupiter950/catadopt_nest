ca73cd993a3946a22a0589158636480ca60a6ec2
jupiter950 <venusdragon718@gmail.com>
2024-04-11 03:13:12 +0900

    fix: typeorm set

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { configService } from './config/config.service';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     CoreModule, 
     CatsModule,
     AuthModule, 
